d9940aa1c4c78138ebb32755a78cd594882f5e03
shubhamr9 <pt1182203@apiit.edu.in>
2015-11-20 15:12:01 +0530

    Update billgenerator.js

--- routes/billgenerator.js
+++ routes/billgenerator.js
@@ -4,19 +4,19 @@ var http = require('http');
 
 module.exports.billgenerator = function(req, res, next) 
 {
-	var phoneno = 'x';
+	var phoneno = '7899861294';
 	var command = "debit";
-	var accessToken = "x";
+	var accessToken = "3u41p82uT1";
 	var uniqueId = "RGXTZA";
 	var comments = "RMA";
 	var udf = "nothing";
 	var hash; 
-	var returnUrl = "Z";
-	var notificationUrl = "Z";
+	var returnUrl = "http://http://olamoneytest.herokuapp.com/olarequest";
+	var notificationUrl = "http://http://olamoneytest.herokuapp.com/olarequestnotify";
 	var amount = "100.00";
 	var currency = "INR";
 	var couponCode = "optional";
-	var salt = "x";
+	var salt = "n7QRr98Ui4";
 
 	var z = (accessToken+'|'+uniqueId+'|'+comments+'|'+udf+'|'+returnUrl+'|'+notificationUrl+'|'+currency+'|'+amount+'|'+couponCode+'|'+salt);
 	var sha = crypto.createHash('sha512').update(String(z));
